3cdf1bcc499b027b7069e59818492a49e49e182c
BuGalter <wardoc78@gmail.com>
2021-12-29 21:01:23 +0300

    Added to countHolesNumber code

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-function coutHolesNumber(currentNumber: number): number {
-  return
+function countHolesNumber(currentNumber: number): number {
+  let currentNumberStr: string = String(currentNumber);
+  let numberHoles: number = currentNumberStr.length;
+  for (let i:number; i < currentNumberStr.length; i += 1) {
+    if (currentNumberStr[i] === '8') {
+      numberHoles += 1;
+    };
+  };
+  return numberHoles;
 };
 
 function sortNumbersHoles(inputArray: number[]): number[] {
