c55a356e7110e95faedf8e6420848881fc9ad8fd
BuGalter <wardoc78@gmail.com>
2021-12-29 21:27:10 +0300

    Added to sortNumberHoles code

--- index.ts
+++ index.ts
@@ -9,6 +9,21 @@ function countHolesNumber(currentNumber: number): number {
   return numberHoles;
 };
 
-function sortNumbersHoles(inputArray: number[]): number[] {
-  return
+function sortNumbersHoles(sortArray: number[]): number[] {
+  let tempArray: Array<number[]> = [];
+  for (let i: number; i < sortArray.length; i += 1) {
+    let numHoles: number = countHolesNumber(sortArray[i]);
+    if (tempArray[numHoles] === undefined) {
+      tempArray[numHoles] = [sortArray[i]];
+    };
+    tempArray[numHoles].push(sortArray[i]);
+  };
+  for (let j: number; j < tempArray.length; j += 1) {
+    if (tempArray[j] !== undefined) {
+      for (let k: number; k < tempArray[j].length; k += 1) {
+        sortArray.push(tempArray[j][k]);
+      };
+    };
+  };
+  return sortArray;
 };
