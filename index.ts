7453aabee5871df40d199c348df9cdced37a1cd1
BuGalter <wardoc78@gmail.com>
2021-12-29 22:06:16 +0300

    Added test data to index.ts and added start values to counters

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 function countHolesNumber(currentNumber: number): number {
   let currentNumberStr: string = String(currentNumber);
+  //console.log(currentNumberStr);
   let numberHoles: number = currentNumberStr.length;
-  for (let i:number; i < currentNumberStr.length; i += 1) {
+  for (let i:number = 0; i < currentNumberStr.length; i += 1) {
     if (currentNumberStr[i] === '8') {
       numberHoles += 1;
     };
@@ -11,19 +12,26 @@ function countHolesNumber(currentNumber: number): number {
 
 function sortNumbersHoles(sortArray: number[]): number[] {
   let tempArray: Array<number[]> = [];
-  for (let i: number; i < sortArray.length; i += 1) {
+  for (let i: number = 0; i < sortArray.length; i += 1) {
     let numHoles: number = countHolesNumber(sortArray[i]);
     if (tempArray[numHoles] === undefined) {
       tempArray[numHoles] = [sortArray[i]];
+      continue;
     };
     tempArray[numHoles].push(sortArray[i]);
   };
-  for (let j: number; j < tempArray.length; j += 1) {
+  sortArray = [];
+  for (let j: number = 0; j < tempArray.length; j += 1) {
     if (tempArray[j] !== undefined) {
-      for (let k: number; k < tempArray[j].length; k += 1) {
+      for (let k: number = 0; k < tempArray[j].length; k += 1) {
         sortArray.push(tempArray[j][k]);
       };
     };
   };
   return sortArray;
 };
+
+let testArray: number[] = [88, 444, 400, 666, 999, 44, 4];
+
+console.log(testArray);
+console.log(sortNumbersHoles(testArray));
